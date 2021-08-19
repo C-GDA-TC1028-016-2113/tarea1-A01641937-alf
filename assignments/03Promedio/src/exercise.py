d462599ea34a6f739f9ae655406b201625db319b
A01641937-alf <A01641937@itesm.mx>
2021-08-19 01:33:18 +0000

    Actividad: programas que realizan calculos

--- assignments/03Promedio/src/exercise.py
+++ assignments/03Promedio/src/exercise.py
@@ -1,6 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    materia1 = int(input("Calificación de la materia: "))
+    materia2 = int(input("Calificación de la materia: "))
+    materia3 = int(input("Calificación de la materia: "))
+    materia4 = int(input("Calificación de la materia: "))
+    promedio = (materia1 + materia2 + materia3 + materia4) / 4
+    print("El promedio es:",promedio)
+
 
 if __name__ == '__main__':
     main()
